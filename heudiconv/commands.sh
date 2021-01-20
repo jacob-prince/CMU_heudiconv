a1ad713a61eef918aa7488156b366035c1e9dff9
jacob-prince <jacob.samuel.prince@gmail.com>
2021-01-20 16:47:07 -0500

    added commments, removed unnecessary vars

--- heudiconv/commands.sh
+++ heudiconv/commands.sh
@@ -1,41 +1,50 @@
 #/bin/bash -f
 
-matlab=/Applications/MATLAB_R2018a.app/bin/matlab
-basedir=/Volumes/jacobprince_external/TarrLab/Projects/Project-BIDS/git/
-outputdir="$basedir"/heudiconv
-sourcedata="$basedir"/sourcedata
-bidsdir=/Volumes/jacobprince_external/TarrLab/Projects/Project-BIDS/git/BIDS/
+matlabpath=/Applications/MATLAB_R2018a.app/bin/matlab # path to matlab installation
+basedir=/Volumes/jacobprince_external/TarrLab/Projects/Project-BIDS/take7 # path to top level project directory
+sourcedatadir="$basedir"/sourcedata # path to directory containing all folders of DICOM data
 
-echo $sourcedata
+echo $sourcedatadir
 
-for dir in $sourcedata
+for dir in $sourcedatadir # iterate through folders containing DICOM data
 do
-    for subdir in "$dir"/*
+    for subdir in "$dir"/* # iterate through subdirectories (each should contain data from 1 subject)
     do
-        if [[ -d $subdir ]]
+        if [[ -d $subdir ]] # if subject is member of "includes" [todo], run heudiconv [reminder to include info about weird folder order in documentation]
         then
             echo $subdir
             subname=$(basename $subdir)
             echo $subname
-            $matlab -nodisplay -nosplash -nojvm -r "renameDicomSeries('${sourcedata}/$subname'); quit;"
 
-            #generate heuristic file
-            # docker run --rm -it \
-            # -v $basedir:/base \
-            # nipy/heudiconv:latest \
-            # -d /base/sourcedata/{subject}/*/*.dcm \
-            # -s $subname \
-            # -f /base/heudiconv/heuristics/convertall.py \
-            # -c none \
-            # -o /base/heudiconv/ \
-            # --overwrite
-            # # #
-            # docker run --rm -it -v $basedir:/base nipy/heudiconv:latest \
-            # -d /base/sourcedata/{subject}/*/*.dcm \
-            # -o /base/BIDS-test/ -f /base/heudiconv/heuristics/heuristic.py \
-            # -s $subname -ss 001 -c dcm2niix -b --overwrite
+            # run matlab function to automatically rename DICOM folders to reflect their contents
+            $matlabpath -nodisplay -nosplash -nojvm -r "renameDicomSeries('${sourcedata}/$subname'); quit;"
 
-            # $matlab -nodisplay -nosplash -nojvm -r "bids_fill_fmap_jsons('$bidsdir','$subname',0,{'pictures','rest'}); quit;"
+            # run setup routine to generate heudiconv ....
+            docker run --rm -it \
+            -v $basedir:/base \
+                nipy/heudiconv:latest \
+            -d /base/sourcedata/{subject}/*/*.dcm \
+            -s $subname \
+            -f /base/heudiconv/heuristics/convertall.py \
+            -c none \
+            -o /base/heudiconv/ \
+            --overwrite
+
+            # run heudiconv conversion using dcm2niix helpers and user-defined heuristic.py file
+            # [todo make functional for multiple sessions]
+            docker run --rm -it \
+            -v $basedir:/base \
+                nipy/heudiconv:latest \
+            -d /base/sourcedata/{subject}/*/*.dcm \
+            -s $subname -ss 01 \
+            -f /base/heudiconv/heuristics/heuristic.py \
+            -c dcm2niix -b \
+            -o /base/BIDS/ \
+            --overwrite
+
+            # to address known issues conforming to BIDS specification, run MATLAB routine
+            # to (1) add "IntendedFor" field to fmap json files and (2) add CogAtlasID fields to func json files
+            # $matlabpath -nodisplay -nosplash -nojvm -r "bids_fill_fmap_jsons('$bidsdir','$subname',0,{'pictures','rest'}); quit;"
 
         fi
     done
